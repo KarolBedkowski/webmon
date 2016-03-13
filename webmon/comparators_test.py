e970b4b794a69323d737b5018257c2e39453b33f
Karol Będkowski <karol.bedkowski@gmail.com>
2016-03-13 22:35:45 +0100

    fix tests

--- webmon/comparators_test.py
+++ webmon/comparators_test.py
@@ -1,112 +1,112 @@
 import unittest
 
-import comparators
+from . import comparators
 
 
 class TestComaratorAdded(unittest.TestCase):
     def test_empty_prev(self):
         curr = [1, 2, 3, 4]
         prev = []
-        diff = comparators.added(prev, None, curr, None)
-        self.assertEqual(diff, "1\n2\n3\n4")
+        diff = list(comparators.Added().format(prev, None, curr, None))
+        self.assertEqual(diff, [1, 2, 3, 4])
 
     def test_empty_curr(self):
         curr = []
         prev = [1, 2, 3, 4]
-        diff = comparators.added(prev, None, curr, None)
-        self.assertEqual(diff, "")
+        diff = list(comparators.Added().format(prev, None, curr, None))
+        self.assertEqual(diff, [])
 
     def test_added_1(self):
         curr = [1, 2, 3, 4, 5]
         prev = [1, 2, 3]
-        diff = comparators.added(prev, None, curr, None)
-        self.assertEqual(diff, "4\n5")
+        diff = list(comparators.Added().format(prev, None, curr, None))
+        self.assertEqual(diff, [4, 5])
 
     def test_added_2(self):
         curr = [1, 2, 3, 4, 5]
         prev = [2, 3]
-        diff = comparators.added(prev, None, curr, None)
-        self.assertEqual(diff, "1\n4\n5")
+        diff = list(comparators.Added().format(prev, None, curr, None))
+        self.assertEqual(diff, [1, 4, 5])
 
     def test_added_3(self):
         curr = [1, 2, 3, 4, 5]
         prev = [2, 4, 5]
-        diff = comparators.added(prev, None, curr, None)
-        self.assertEqual(diff, "1\n3")
+        diff = list(comparators.Added().format(prev, None, curr, None))
+        self.assertEqual(diff, [1, 3])
 
     def test_added_4(self):
         curr = [1, 2, 3, 4, 5]
         prev = [2, 4]
-        diff = comparators.added(prev, None, curr, None)
-        self.assertEqual(diff, "1\n3\n5")
+        diff = list(comparators.Added().format(prev, None, curr, None))
+        self.assertEqual(diff, [1, 3, 5])
 
     def test_added_5(self):
         curr = [1, 6]
         prev = [1, 2, 3, 4, 5]
-        diff = comparators.added(prev, None, curr, None)
-        self.assertEqual(diff, "6")
+        diff = list(comparators.Added().format(prev, None, curr, None))
+        self.assertEqual(diff, [6])
 
     def test_added_6(self):
         curr = [6, 7]
         prev = [1, 2, 3, 4, 5]
-        diff = comparators.added(prev, None, curr, None)
-        self.assertEqual(diff, "6\n7")
+        diff = list(comparators.Added().format(prev, None, curr, None))
+        self.assertEqual(diff, [6, 7])
 
 
 class TestComaratorDeleted(unittest.TestCase):
     def test_empty_prev(self):
         curr = [1, 2, 3, 4]
         prev = []
-        diff = comparators.deleted(prev, None, curr, None)
-        self.assertEqual(diff, "")
+        diff = list(comparators.Deleted().format(prev, None, curr, None))
+        self.assertEqual(diff, [])
 
     def test_empty_curr(self):
         curr = []
         prev = [1, 2, 3, 4]
-        diff = comparators.deleted(prev, None, curr, None)
-        self.assertEqual(diff, "1\n2\n3\n4")
+        diff = list(comparators.Deleted().format(prev, None, curr, None))
+        self.assertEqual(diff, [1, 2, 3, 4])
 
     def test_deleted_1(self):
         curr = [1, 2, 3, 4, 5]
         prev = [1, 2, 3]
-        diff = comparators.deleted(prev, None, curr, None)
-        self.assertEqual(diff, "")
+        diff = list(comparators.Deleted().format(prev, None, curr, None))
+        self.assertEqual(diff, [])
 
     def test_deleted_2(self):
         curr = [1, 2, 3, 4, 5]
         prev = [2, 3]
-        diff = comparators.deleted(prev, None, curr, None)
-        self.assertEqual(diff, "")
+        diff = list(comparators.Deleted().format(prev, None, curr, None))
+        self.assertEqual(diff, [])
 
     def test_deleted_3(self):
         curr = [1, 2, 3, 4, 5]
         prev = [2, 4, 5]
-        diff = comparators.deleted(prev, None, curr, None)
-        self.assertEqual(diff, "")
+        diff = list(comparators.Deleted().format(prev, None, curr, None))
+        self.assertEqual(diff, [])
 
     def test_deleted_4(self):
         curr = [1, 2, 3, 4, 5]
         prev = [2, 4]
-        diff = comparators.deleted(prev, None, curr, None)
-        self.assertEqual(diff, "")
+        diff = list(comparators.Deleted().format(prev, None, curr, None))
+        self.assertEqual(diff, [])
 
     def test_deleted_5(self):
         curr = [1, 6]
         prev = [1, 2, 3, 4, 5]
-        diff = comparators.deleted(prev, None, curr, None)
-        self.assertEqual(diff, "2\n3\n4\n5")
+        diff = list(comparators.Deleted().format(prev, None, curr, None))
+        self.assertEqual(diff, [2, 3, 4, 5])
 
     def test_deleted_6(self):
         curr = [6, 7]
         prev = [1, 2, 3, 4, 5]
-        diff = comparators.deleted(prev, None, curr, None)
-        self.assertEqual(diff, "1\n2\n3\n4\n5")
+        diff = list(comparators.Deleted().format(prev, None, curr, None))
+        self.assertEqual(diff, [1, 2, 3, 4, 5])
 
     def test_deleted_7(self):
         curr = [1, 5]
         prev = [1, 2, 3, 4, 5]
-        diff = comparators.deleted(prev, None, curr, None)
-        self.assertEqual(diff, "2\n3\n4")
+        diff = list(comparators.Deleted().format(prev, None, curr, None))
+        self.assertEqual(diff, [2, 3, 4])
 
 if __name__ == '__main__':
     unittest.main()
--- webmon/config_test.py
+++ webmon/config_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-import config
+from . import config
 
 
 class TestApplyDefaults(unittest.TestCase):
