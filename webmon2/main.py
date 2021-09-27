b5c645787a2a813bcec332d39af8f1477d9e8ac4
Karol Będkowski <karol.bedkowski@gmail.com>
2021-09-27 17:23:45 +0200

    bump: webmon2 2.5.6 2021-09-27

--- webmon2/main.py
+++ webmon2/main.py
@@ -38,7 +38,7 @@ __author__ = "Karol Będkowski"
 __copyright__ = "Copyright (c) Karol Będkowski, 2016-2021"
 _ = ty
 
-VERSION = "2.5.5"
+VERSION = "2.5.6"
 APP_NAME = "webmon2"
 
 _LOG = logging.getLogger("main")
