63517fb527c7cd6f5124359e6edceed96687109d
Karol Będkowski <karol.bedkowski@gmail.com>
2021-11-08 16:48:37 +0100

    chore(db): enable lz4 compression in Postgresql 14+

--- webmon2/schema/0000022.sql
+++ webmon2/schema/0000022.sql
@@ -9,4 +9,12 @@ CREATE INDEX IF NOT EXISTS entries_idx2 ON public.entries USING btree (source_id
 
 ALTER TABLE source_state RENAME COLUMN state TO props;
 
+-- in pg >= 14 enable lz4 compression
+DO $$
+BEGIN
+	IF  current_setting('server_version_num')::int >= 140000 THEN
+		ALTER TABLE entries ALTER COLUMN content SET COMPRESSION lz4;
+	END IF;
+END $$;
+
 -- vim:et
