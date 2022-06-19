602a2dc6121f16b5ce15ee32fa22740eeb7dcfaa
Karol Będkowski <karol.bedkowski@gmail.com>
2022-06-19 22:16:49 +0200

    chore(proxy): clean code

--- webmon2/web/proxy.py
+++ webmon2/web/proxy.py
@@ -7,7 +7,7 @@
 # Distributed under terms of the GPLv3 license.
 
 """
-
+Proxy request via webmon application.
 """
 
 import logging
@@ -19,24 +19,24 @@ from flask import Blueprint, Response, abort, request
 _LOG = logging.getLogger(__name__)
 BP = Blueprint("proxy", __name__, url_prefix="/proxy")
 
+_EXCLUDED_HEADERS = [
+    "content-encoding",
+    "content-length",
+    "transfer-encoding",
+    "connection",
+]
+
 
 @BP.route("/<path:path>", methods=["GET"])
 def proxy(path: str) -> ty.Any:
-    if request.method != "GET":
-        abort(401)
+    _LOG.debug("proxy request to: %s", path)
 
     resp = requests.get(path)
-    excluded_headers = [
-        "content-encoding",
-        "content-length",
-        "transfer-encoding",
-        "connection",
-    ]
     headers = [
         (name, value)
         for (name, value) in resp.raw.headers.items()
-        if name.lower() not in excluded_headers
+        if name.lower() not in _EXCLUDED_HEADERS
     ]
-    response = Response(resp.content, resp.status_code, headers)
 
-    return response
+    _LOG.debug("proxy request result: status: %r", resp.status_code)
+    return Response(resp.content, resp.status_code, headers)
